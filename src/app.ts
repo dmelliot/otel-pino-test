a70e50c0d77e084a7aa7c8666ef266ad9c55a026
Duncan Elliot <dmelliot@gmail.com>
2024-05-12 22:12:55 +1000

    Small cleanup

--- src/app.ts
+++ src/app.ts
@@ -3,28 +3,13 @@ import { logger } from "./logger";
 import { open } from "fs";
 import { functionWithNoOtelVisibility } from "./otherFile";
 
-const deepFunction1 = () => {
-  logger.info("logging from a deeper function");
-  deepFunction2();
-};
-
-const deepFunction2 = () => {
-  logger.info("logging from two levels deep");
-};
-
 logger.info("foobara");
 
 const tracer = opentelemetry.trace.getTracer("example-basic-tracer-node");
 
-const s = tracer.startSpan("my space");
-s.setAttribute("criterion", "sdfsdsdfsdf");
-
-logger.info(`maybe here?`);
 for (let i = 0; i < 1; i++) {
   const val = tracer.startActiveSpan("init", (span) => {
-    span.setAttribute("criterion", "mu-criterion-urn");
     logger.info(`expect trace_id to be in this log ${i}`);
-    deepFunction1();
     functionWithNoOtelVisibility();
     span.end();
     return false;
